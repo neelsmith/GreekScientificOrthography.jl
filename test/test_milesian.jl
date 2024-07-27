be5e09e79cc4075e3be68acb6e5ee34574c9ec6d
Neel Smith <dnsmith.neel@gmail.com>
2024-07-27 08:43:19 -0400

    Fail tests on integer values

--- test/test_milesian.jl
+++ test/test_milesian.jl
@@ -1,4 +1,17 @@
 
-@testset "Test conversion of numeric values" begin
+@testset "Test parsing integer values" begin
+    @test_broken milesian("Ο$(NUMERIC_TICK)") == 0
+    @test_broken milesian("α$(NUMERIC_TICK)") == 1
+    @test_broken milesian("ρα$(NUMERIC_TICK)") == 101
+    @test_broken milesian("ρια$(NUMERIC_TICK)") == 111
+    @test_broken milesian("α,$(NUMERIC_TICK)") == 1000
+    @test_broken milesian("α,α$(NUMERIC_TICK)") == 1001
+
+    @test_broken milesian("θ,ϡϙθ$(NUMERIC_TICK)") == 9999
+    @test_broken milesian("Μ$(NUMERIC_TICK)") == 10000
     
+    @test_broken milesian("Μ$(NUMERIC_TICK) α$(NUMERIC_TICK)") == 10001
+    @test_broken milesian("Μ$(NUMERIC_TICK) θ,ϡϙθ$(NUMERIC_TICK)") == 19999
+    @test_broken milesian("Μ^β^") == 20000
+    @test_broken milesian("Μ^β^ α,$(NUMERIC_TICK)") == 21001
 end
