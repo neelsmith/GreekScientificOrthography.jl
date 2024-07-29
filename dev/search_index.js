84c319ac5919d63f292e1090928e6505cc1aa18f
Documenter.jl <documenter@juliadocs.github.io>
2024-07-29 16:10:10 +0000

    build based on 61d67b8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GreekScientificOrthogaphy.jl","page":"Documentation","title":"GreekScientificOrthogaphy.jl","text":"","category":"section"},{"location":"","page":"Documentation","title":"Documentation","text":"Documentation is availabe on quarto pub.","category":"page"}]
+}
